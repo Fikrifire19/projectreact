92937a79add69bdf3e738c8e9a4bfe0ddc65f378
Fikrifire19 <muhamadfikri64@yahoo.co.id>
2020-09-11 23:41:55 +0700

    membuat router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './views/Home';
+import About from './views/About';
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <div className="p-3">
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </div>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/views/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h1 className="font-bold font-2xl">This Is About Page</h1>
+        </div>
+    );
+}
+
+export default About;
